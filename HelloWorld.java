6a3a298e125c9959c159135ee7c1b4764fa8a478
Xanosite <73138502+Xanosite@users.noreply.github.com>
2021-01-05 10:54:47 -0800

    CS110 Lab 1: HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+//  Author: Blake Hershberger
+//  Date: 20210104
+//  File: HelloWorld.java
+
+//  Class? IDK what's with java and class being the main container.
+class HelloWorld {
+  //  This is more like it, though if the main function returns void, how do you know it executed properly?
+  //  What's going on with the arguments? Passed an empty string?(array?list?) and the word args.
+  //  Why is System and String capitalized?
+  public static void main(String[] args) {
+    //  Is "System" a library? Familiar to python calling library functions.
+    //  println implies to existence of other, better, console output options
+    System.out.println("Hello, World!");
+  }
+}
