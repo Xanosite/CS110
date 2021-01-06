7def6e7f869d8bf70a60d86516a5ab892ce679b5
Xanosite <blake.lee.hershberger@gmail.com>
2021-01-06 11:35:46 -0800

    First homework assignemnt

---
+++ TodayIs.java
@@ -0,0 +1,13 @@
+//  Author: Blake Hershberger
+//  Date: 20210106
+//  File: TodayIs.java
+
+class TodayIs {
+  public static void main (String[] args) {
+    //today is not the 7th of September
+    int dayInt = 7, monthInt = 9;
+    System.out.println("The month is September");
+    System.out.println("September is month number " + monthInt);
+    System.out.println("Today is the " + dayInt + "th day of the month");
+  }
+}
